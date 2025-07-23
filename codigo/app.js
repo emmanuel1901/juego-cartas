e392f9b1a7e4df74a8fd08ef93407574dd256b35
Emmanuel <emmanuel.montoya@udea.edu.cogit config --global user.email emmanuel.montoya@udea.edu.co>
2025-07-23 12:51:31 -0500

    se realizaron avances en la funcion verificar

--- codigo/app.js
+++ codigo/app.js
@@ -1,10 +1,14 @@
+let repartio = false;
+let cartas = []
+
 function repartir() {
     let tablero = document.getElementById("tablero");
-    tablero.innerHTML="";
+    tablero.innerHTML = "";
     const TOTAL_CARTAS = 10;
     for (let i = 0; i < TOTAL_CARTAS; i++) {
         let carta = document.createElement("img");
         let indice = Math.floor(Math.random() * 52) + 1;
+        cartas.push(indice);//push pone datos a un vector
         carta.src = "imagenes/cartas/Carta" + indice + ".jpg";
         tablero.appendChild(carta);
     }
@@ -12,9 +16,32 @@ function repartir() {
     audio.play().then(() => {
         window.alert("Se han repartido las 10 cartas");
     });
-
+    repartio = true;
 }
 
-function verificar(){
-    window.alert("Mañana chequearemos las cartas");
+function verificar() {
+    if (!repartio) {//! esto nos indicas que partimos del falso
+        window.alert("No se han repartido")
+    }
+    else {
+        //iniciar los contadores en cero 
+        contadores = new Array(13).fill(0);
+        //recorrer la lista de las cartas
+        for (let i = 0; i < cartas.length; i++) {
+            posicion = cartas[i] % 13;
+            if (posicion = 0) {
+                posicion = 12;
+            }
+            else {
+                posicion--;
+            }
+            contadores[posicion]++;
+        }
+        nombreCartas = ["As", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "jack", "queen", "king"];
+        //recorrer los contadores
+        for (let i = 0; i < contadores.length; i++) {
+            window.alert("hay " + contadores[i] + " cartas de " + nombreCartas[i]);
+
+        }
+    }
 }
